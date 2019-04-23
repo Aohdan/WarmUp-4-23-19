6de8745610090fd63ab9d7634902fbdad2b68357
Aohdan <48027632+Aohdan@users.noreply.github.com>
2019-04-23 09:46:25 -0700

    Add files via upload

--- 4-23-19.php
+++ 4-23-19.php
@@ -5,6 +5,23 @@
     <title>4-23-19</title>
 </head>
 <body>
-    
+    <?php
+    echo"<h1>Marec Szpor</h1>";
+    $limit = 4000000;
+    function evenSum($limit) {
+        if ($limit < 2)
+            return 0;
+        $n1 = 1;
+        $n2 = 2;
+        $sum = $n1 + $n2;
+        while ($n2 <= $limit){
+            $n3 = 4 * $n2 + $n1;
+            if ($n3 > $limit)
+                break;
+        }
+            return $sum;
+    }
+    echo"(evenSum($limit));";
+    ?>
 </body>
 </html>
